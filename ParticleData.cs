a6657fc2c827d96b40c72bd9673ecf75a204f527
zhifeigithub <zhifeiemail@gmail.com>
2017-12-06 14:13:48 +0800

    Initial commit

---
+++ ParticleData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleData
+{
+    public Vector3 position;
+    public float size;
+    public Vector3 rotation;
+    public Color color;
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# unity_function_codes
